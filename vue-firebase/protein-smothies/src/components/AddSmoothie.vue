aac456c863d5b1d5fd7a09a4db25c96d7885a9ed
Cesar Gomez <zootechdrum@gmail.com>
2019-10-04 18:05:35 -0700

    added form

--- vue-firebase/protein-smothies/src/components/AddSmoothie.vue
+++ vue-firebase/protein-smothies/src/components/AddSmoothie.vue
@@ -1,5 +1,20 @@
 <template>
-  <h2>Add Smoothie</h2>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form>
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient</label>
+        <input type="text" name="add-ingredient">
+      </div>
+      <div class="field center-align">
+        <button class="btn pink">Add smoothie</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -9,9 +24,27 @@ export default {
   name: 'AddSmoothie',
   data(){
     return {
-
+      title:null
     }
   }
 }
 </script>
+
+<style>
+.add-smoothie {
+  margin-top: 60px;
+  padding:20px;
+  max-width: 500px;
+}
+
+.add-smoothie h2 {
+  font-size:2em;
+  margin: 20px auto;
+}
+
+.add-smoothie .field {
+  margin: 20px auto;
+}
+</style>
+
 
