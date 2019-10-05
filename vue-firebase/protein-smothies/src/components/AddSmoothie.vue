539ce14703e147623abb3da56c382304132473c0
Cesar Gomez <zootechdrum@gmail.com>
2019-10-04 17:48:23 -0700

    added delete functionality to firestire database

---
+++ vue-firebase/protein-smothies/src/components/AddSmoothie.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Add Smoothie</h2>
+</template>
+
+<script>
+
+
+export default {
+  name: 'AddSmoothie',
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
--- vue-firebase/protein-smothies/src/components/Index.vue
+++ vue-firebase/protein-smothies/src/components/Index.vue
@@ -15,47 +15,50 @@
 </template>
 
 <script>
-
-import db from '@/firebase/init'
+import db from "@/firebase/init";
 
 export default {
-  name: 'Index',
-  data () {
+  name: "Index",
+  data() {
     return {
-      smoothies: [ ]
-    }
+      smoothies: []
+    };
   },
-  methods:{
+  methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id !== id;
-      })
+      //delete doc from firestore
+      db
+        .collection("smoothies").doc(id).delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id !== id;
+          });
+        });
     }
   },
   created() {
-    db.collection("smoothies").get()
+    db
+      .collection("smoothies")
+      .get()
       .then(snapshot => {
         snapshot.forEach(doc => {
-          console.log(doc.data())
-          let smoothie = doc.data()
-          smoothie.id = doc.id
-          this.smoothies.push(smoothie)
-        })
-      }
-    )
+          console.log(doc.data());
+          let smoothie = doc.data();
+          smoothie.id = doc.id;
+          this.smoothies.push(smoothie);
+        });
+      });
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-
 .index {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 30px;
   margin-top: 60px;
-
 }
 
 .index h2 {
@@ -63,20 +66,19 @@ export default {
   font-size: 1.8rem;
 }
 
-.index .ingrerdients{
+.index .ingrerdients {
   margin-top: 30px auto;
 }
 
 .index .ingrerdients li {
-  display:inline-block;
+  display: inline-block;
 }
 
 .index .delete {
-  position:absolute;
-  top:4px;
+  position: absolute;
+  top: 4px;
   right: 4px;
   font-size: 1.4em;
   cursor: pointer;
 }
-
 </style>
